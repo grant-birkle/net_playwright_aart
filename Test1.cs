f673013b5dd33a5dfb0e4fe3a4917409225d895a
grant-birkle <109991006+grant-birkle@users.noreply.github.com>
2025-06-22 20:05:27 -0500

    Add files via upload

---
+++ Test1.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Playwright;
+using Microsoft.Playwright.MSTest;
+
+namespace PlaywrightTests;
+
+[TestClass]
+public class ExampleTest : PageTest
+{
+    [TestMethod]
+    public async Task AART_MainPageTitle()
+    {
+        await Page.GotoAsync("https://www.arrt.org/");
+        // Verify AART's Title
+        await Expect(Page).ToHaveTitleAsync("Welcome to the American Registry of Radiologic Technologists (ARRT) website - ARRT.org - ARRT");
+
+    }
+
+    [TestMethod]
+    public async Task AART_AboutPage()
+    {
+        await Page.GotoAsync("https://www.arrt.org/");
+
+        // Select About dropdown:
+        await Page.Locator("[data-toggle='dropdown']").First.ClickAsync();
+
+        // Select the About Us item
+        await Page.GetByRole(AriaRole.Link, new() { Name = "About Us" }).ClickAsync();
+
+        // // Verify About page has correct URL:
+        await Expect(Page).ToHaveURLAsync("https://www.arrt.org/pages/about/about-us");
+
+        // // verify About Page Title:
+        await Expect(Page).ToHaveTitleAsync("Welcome to the American Registry of Radiologic Technologists (ARRT) website - ARRT.org - ARRT");
+        var title = await Page.TitleAsync();
+        Console.WriteLine("Ttitle: " + title); 
+    }    
+}
